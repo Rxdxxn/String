e19d89894c817294e95db1554ea2a265940571da
Rodion Balaniuc <70860169+Rxdxxn@users.noreply.github.com>
2020-11-26 12:09:35 +0300

    Add files via upload

---
+++ RB5.py
@@ -0,0 +1,6 @@
+n1=str(input("Dati primul cuvant:"))
+n2=str(input("Dati al doilea cuvant:"))
+n3=str(input("Dati al treilea cuvant:"))
+n4=str(input("Dati al patrulea cuvant:")) 
+s=n1 +" " +n2 +" " +n3 +" " +n4
+print("fraza formata este:" , s,".")         
